1fda40b70978814074c800e1eae93447faf94438
abhinayreddykommera <abhinayreddykommera>
2019-08-27 23:24:34 -0400

    initial commit

---
+++ src/com/abhi/Main.java
@@ -0,0 +1,18 @@
+package com.abhi;
+class Car {
+    private String engine;
+    private int cylinders;
+    private int wheels;
+
+    public Car(String engine, int cylinders, int wheels) {
+        this.engine = "Isuzu";
+        this.cylinders = 2;
+        this.wheels = 4;
+    }
+}
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
